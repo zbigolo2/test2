a6c32f3e758fda312b3a76509bbc08272caf6075
zbigolo2 <132271271+zbigolo2@users.noreply.github.com>
2023-05-01 16:27:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function (){
+    var login = document.getElementById('logowanie');
+    login.addEventListener('click',function(){
+        alert('Strona w trakcie budowy');
+    })
+})
